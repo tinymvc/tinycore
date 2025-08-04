4f7f6241adaa7771cea04922b60012c86390aa43
Shahin Moyshan <freelance.shain@gmail.com>
2025-08-04 23:04:25 +0600

    Update user() method return type to mixed for improved type clarity

--- src/Http/Auth.php
+++ src/Http/Auth.php
@@ -123,9 +123,9 @@ class Auth implements AuthContract, ArrayAccess
      *
      * @param string|null $key The key to retrieve from the user model, or null to return the entire model.
      * @param mixed $default The default value to return if the key does not exist.
-     * @return false|Model The user model or false if not logged in, or the value of the specified key.
+     * @return mixed The user model or false if not logged in, or the value of the specified key.
      */
-    public function user(?string $key = null, $default = null): false|Model
+    public function user(?string $key = null, $default = null): mixed
     {
         if ($key !== null && !$this->isGuest()) {
             return $this->getUser()->get($key, $default);
